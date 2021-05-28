b3e97c2c12e111faceb0c5f19144379a0ed04fdc
Jorge Andrés Mena <jormencar@yahoo.com>
2021-05-28 16:48:51 -0400

    first coommit

---
+++ src/pages/index.js
@@ -0,0 +1,31 @@
+import React from "react"
+import { StaticImage } from "gatsby-plugin-image"
+import Layout from "../components/Layout"
+import AllRecipes from "../components/AllRecipes"
+import SEO from "../components/SEO"
+
+export default function Home() {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <main className="page">
+        <header className="hero">
+          <StaticImage
+            src="../assets/images/main.jpeg"
+            alt="eggs"
+            className="hero-img"
+            placeholder="tracedSVG"
+            layout="fullWidth"
+          />
+          <div className="hero-container">
+            <div className="hero-text">
+              <h1>simply recipes</h1>
+              <h4>no fluff, Just Recipes</h4>
+            </div>
+          </div>
+        </header>
+        <AllRecipes />
+      </main>
+    </Layout>
+  )
+}
